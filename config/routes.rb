48030a72bd05b0d89d9a5d6897b1d1d20089152d
Federico Gallardo <fedegallar2006@hotmail.com>
2020-09-30 00:33:21 -0300

    Routes and Bootstraping

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :tareas
   resources :materia
   resources :estados
+  root to: "tareas#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
